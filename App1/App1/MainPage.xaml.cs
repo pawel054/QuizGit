8badbc09fefc430f7584ddbaeae733d2cbce0408
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-24 18:57:50 +0100

    Add StartQuiz function in main xaml.cs

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -28,5 +28,23 @@ namespace App1
             Random random = new Random();
             questions = Enumerable.Range(0,5).Select(_=> random.Next(1,1001)).ToList();
         }
+
+        private void StartQuiz(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(userNameEntry.Text))
+            {
+                DisplayAlert("Alert", "pole nie może być puste!", "OK");
+            }
+            else
+            {
+                currentScore = 0;
+                currentQuestionIndex = 0;
+                times.Clear();
+                startGameView.IsVisible = false;
+                finishGameView.IsVisible = false;
+                gameView.IsVisible = true;
+                ShowNextQuestion();
+            }
+        }
     }
 }
