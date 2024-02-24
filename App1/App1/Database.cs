833b8a97037b2c8b7eef09493472c9de641c74f1
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-24 18:47:15 +0100

    Add Database class

--- App1/App1/Database.cs
+++ App1/App1/Database.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace App1
 {
@@ -12,7 +13,36 @@ namespace App1
         public Database(string dbPath)
         {
             database = new SQLiteAsyncConnection(dbPath);
-            database.CreateIndexAsync<UserResult>().Wait();
+            database.CreateTableAsync<UserResult>().Wait();
+        }
+
+        public Task<List<UserResult>> GetResultsAsync()
+        {
+            return database.Table<UserResult>().ToListAsync();
+        }
+
+        public Task<UserResult> GetUserName(string userName) 
+        {
+            return database.Table<UserResult>().Where(i=>i.UserName == userName).FirstOrDefaultAsync();
+        }
+
+        public async Task SaveResultAsync(UserResult result)
+        {
+            var existingResult = await GetUserName(result.UserName);
+
+            if(existingResult != null) 
+            {
+                if(result.Score >  existingResult.Score)
+                {
+                    existingResult.TotalTime = result.TotalTime;
+                    existingResult.Score = result.Score;
+                    await database.UpdateAsync(existingResult);
+                }
+            }
+            else
+            {
+                await database.UpdateAsync(result);
+            }
         }
 
     }
