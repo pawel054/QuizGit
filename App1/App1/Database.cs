9e710db0eb0dd618ce72fb1b54ff06d392c0a92d
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-24 18:42:09 +0100

    Add UserResult class

---
+++ App1/App1/Database.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public class Database
+    {
+        private SQLiteAsyncConnection database;
+
+        public Database(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateIndexAsync<UserResult>().Wait();
+        }
+
+    }
+}
