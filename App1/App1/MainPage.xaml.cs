8004e32aac31a1ae7006ea62513b6bcc24a21093
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-02-24 18:52:16 +0100

    Declare global cariables and lists in main page xaml.cs

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace App1
 {
     public partial class MainPage : ContentPage
     {
+        private int currentQuestionIndex = 0;
+        private int currentScore = 0;
+        private List<int> questions = new List<int>();
+        private Stopwatch stopwatch = new Stopwatch();
+        private List<double> times = new List<double>();
+
         public MainPage()
         {
             InitializeComponent();
